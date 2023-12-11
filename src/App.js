249ff487825c5d5f418b04c48652228ec0eed18c
naatii <natiworlds@gmail.com>
2023-12-11 13:17:49 +0100

    arreglando navegación

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Inicio from "./pages/Inicio"
 import SobreNosotros from "./pages/SobreNosotros"
 import ColeecionNatiworlds from "./pages/ColeecionNatiworlds";
 import './App.css';
-import React from 'react';
-import ColeccionGamer from "./pages/ColeccionGamer";
+import ColeecionGamer from "./pages/ColeccionGamer";
 
 
 function App() {
   return (
-    <BrowserRouter>
+
       <Routes>
-        <Route path="/" element={ <Inicio /> } />
-        <Route index element={ <Inicio /> } />
-        <Route path="sobre-nosotros" element={<SobreNosotros />} />
-        <Route path="coleccion-natiworlds" element={<ColeecionNatiworlds />} />
-        <Route path="coleccion-gamer" element={<ColeccionGamer />} />
-        {/* <Route path="contacto" element={<Contacto />} /> */}
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobre-nosotros" element={<SobreNosotros />} />
+        <Route path="/coleccion-natiworlds" element={<ColeecionNatiworlds />} />
+        <Route path="/coleccion-gamer" element={<ColeecionGamer />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
